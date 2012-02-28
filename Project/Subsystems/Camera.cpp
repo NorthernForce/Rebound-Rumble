913830eddeb5acc248d50fc00a07be00d94dccdc
arthurlockman <arthurlockman@fd4378a5-d3d7-419b-aeaa-11d43d7afb62>
2012-02-28 21:02:35 +0000

    Fixed camera math to use correct target height.

--- Project/Subsystems/Camera.cpp
+++ Project/Subsystems/Camera.cpp
@@ -82,28 +82,28 @@ float Camera::GetDistanceToTarget() const
 		float d2 = 0.0;
 
 		case AxisCameraParams::kResolution_160x120:
-			d1 = (((k_targetHeight*120)/topTarget.height/)2)/tan(32.625/2));
-			d2 = (((k_targetWidth*160)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
+			d1 = (((k_visionTargetHeight*120)/topTarget.height/)2)/tan(32.625/2));
+			d2 = (((k_visionTargetWidth*160)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
 			return (d1+d2)/2;
 
 		case AxisCameraParams::kResolution_320x240:
-			d1 = (((k_targetHeight*240)/topTarget.height/)2)/tan(32.625/2));
-			d2 = (((k_targetWidth*320)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
+			d1 = (((k_visionTargetHeight*240)/topTarget.height/)2)/tan(32.625/2));
+			d2 = (((k_visionTargetWidth*320)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
 			return (d1+d2)/2;
 			
 		case AxisCameraParams::kResolution_640x480:
-			d1 = (((k_targetHeight*480)/topTarget.height/)2)/tan(32.625/2));
-			d2 = (((k_targetWidth*640)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
+			d1 = (((k_visionTargetHeight*480)/topTarget.height/)2)/tan(32.625/2));
+			d2 = (((k_visionTargetWidth*640)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
 			return (d1+d2)/2;
 			
 		case AxisCameraParams::kResolution_640x360:
-			d1 = (((k_targetHeight*360)/topTarget.height/)2)/tan(32.625/2));
-			d2 = (((k_targetWidth*640)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
+			d1 = (((k_visionTargetHeight*360)/topTarget.height/)2)/tan(32.625/2));
+			d2 = (((k_visionTargetWidth*640)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
 			return (d1+d2)/2;
 			
 		default:
-			d1 = (((k_targetHeight*120)/topTarget.height/)2)/tan(32.625/2));
-			d2 = (((k_targetWidth*160)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
+			d1 = (((k_visionTargetHeight*120)/topTarget.height/)2)/tan(32.625/2));
+			d2 = (((k_visionTargetWidth*160)/topTarget.width)/2)/tan(k_cameraViewAngle/2);
 			return (d1+d2)/2;
 	}
 }
