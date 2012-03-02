8ba5e45f369d4851a8cbc6b2620a05ef967fceee
arthurlockman <arthurlockman@fd4378a5-d3d7-419b-aeaa-11d43d7afb62>
2012-03-02 18:49:37 +0000

    Changes to ShootBall.

--- Project/Commands/ShootBall.cpp
+++ Project/Commands/ShootBall.cpp
@@ -9,31 +9,31 @@ ShootBall::ShootBall(): CommandBase("ShootBall") {
 
 // Called just before this Command runs the first time
 void ShootBall::Initialize() {
-	count=0;
 	s_ballShooter->SetSpeed(.8);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void ShootBall::Execute() {
-	// Wait 25 cycles before we enable the lift
-	if (++count >= 25)
+	// Wait 25 cycles before we. enable the lift
+	if (TimeSinceInitialized() > 1)
 		s_ballLifter->EnableLift();
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool ShootBall::IsFinished() {
-	return false;//s_ballLifter->GetLimit();
+//	return s_ballLifter->GetLimit();
+	return false;
 }
 
 // Called once after isFinished returns true
 void ShootBall::End() {
 	s_ballLifter->DisableLift();
-	s_ballShooter->SetSpeed(0);
+	s_ballShooter->SetSpeed(.2);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void ShootBall::Interrupted() {
 	s_ballLifter->DisableLift();
-	s_ballShooter->SetSpeed(0);
+	s_ballShooter->SetSpeed(.2);
 }
