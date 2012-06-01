ddd8ece5a413a25e9b0082d06fd8abfcacb5b713
arthurlockman <arthurlockman@fd4378a5-d3d7-419b-aeaa-11d43d7afb62>
2012-06-01 22:57:16 +0000

    Repaired StopFiring so that it actually stops firing. Stopped aiming the turret for Beantown Blitz; will re-enable possibly at competition. Removed smart dashboard value for shot speed, as it is being set statically.

--- Project/Commands/AimTurret.cpp
+++ Project/Commands/AimTurret.cpp
@@ -12,32 +12,32 @@ AimTurret::AimTurret() : CommandBase("AimTurret")
 // Called just before this Command runs the first time
 void AimTurret::Initialize() 
 {
-	s_turret->Calibrate();
+	// s_turret->Calibrate();
 }
 
 // Called repeatedly when this Command is scheduled to run
 void AimTurret::Execute() 
 {
-	if (!s_turret->m_calibrated)
-	{
-		s_turret->Calibrate();
-	}
-	else if (s_turret->m_calibrated)
-	{
-//		if (s_camera->HasTarget() && k_track == true) {
-//			if (oi->GetManipulatorStick().GetYButton() || oi->GetStick().GetYButton())
-//				s_turret->SetPosition(k_turretCenter);
-//			else
-//				s_turret->SetPosition(s_camera->GetTurretSetpoint());
-//		} else {
-			if (oi->GetManipulatorStick().GetBButton())
-				s_turret->SetPosition(s_turret->GetPosition()+.05);
-			else if (oi->GetManipulatorStick().GetXButton())
-				s_turret->SetPosition(s_turret->GetPosition()-.05);
-			else if (oi->GetManipulatorStick().GetYButton())
-				s_turret->SetPosition(k_turretCenter);
-//		}
-	}
+	// if (!s_turret->m_calibrated)
+	// {
+	// 	s_turret->Calibrate();
+	// }
+	// else if (s_turret->m_calibrated)
+	// {
+	// 	if (s_camera->HasTarget() && k_track == true) {
+	// 		if (oi->GetManipulatorStick().GetYButton() || oi->GetStick().GetYButton())
+	// 			s_turret->SetPosition(k_turretCenter);
+	// 		else
+	// 			s_turret->SetPosition(s_camera->GetTurretSetpoint());
+	// 	} else {
+	// 		if (oi->GetManipulatorStick().GetBButton())
+	// 			s_turret->SetPosition(s_turret->GetPosition()+.05);
+	// 		else if (oi->GetManipulatorStick().GetXButton())
+	// 			s_turret->SetPosition(s_turret->GetPosition()-.05);
+	// 		else if (oi->GetManipulatorStick().GetYButton())
+	// 			s_turret->SetPosition(k_turretCenter);
+	// 	}
+	// }
 }
 
 // Make this return true when this Command no longer needs to run execute()
--- Project/ReboundRumbleBot.cpp
+++ Project/ReboundRumbleBot.cpp
@@ -167,7 +167,6 @@ protected:
 			
 			SetSmartDashboardDouble ("Turret Setpoint",pCamera->GetTurretSetpoint());
 			SetSmartDashboardDouble ("Turret Position",CommandBase::s_turret->GetPosition());
-			SetSmartDashboardDouble("Shot Speed",fabs(CommandBase::oi->GetManipulatorStick().GetRawAxis(3)));
 
 			float arrivalAngle = -atan(k_tanTheta + 2*k_targetHeight/(pCamera->GetHorizontalDistance()));
 			if( arrivalAngle < k_aAngleMax &&
